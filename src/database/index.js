06b6fb4de759f58bdd41a91c215cbf18bc7e74be
Rafael Bertoldo <rafael@kenzie.com.br>
2022-10-17 11:00:00 -0300

    fix: build command

---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+const {createUser} = require ('./../services/user.services');
+
+
+createUser({
+    username:"kenzinho",
+    email: "kenzinho@mail.com",
+    password:"123456" ,
+    avatar: "https://images.tcdn.com.br/img/img_prod/740836/cadeira_gamer_concordia_gm3_rgb_com_controle_e_powerbank_10803_1_20a776245ed6e9b1bd655072771901e6.png"
+}
+)
+
