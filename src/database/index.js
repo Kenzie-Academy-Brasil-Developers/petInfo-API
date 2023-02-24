2d27874c56634f9b7c59fbd804e41d651a48aabd
Rafael Bertoldo <rafael@kenzie.com.br>
2023-02-24 16:29:28 -0300

    fix:first user image

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ createUser({
     username:"kenzinho",
     email: "kenzinho@mail.com",
     password:"123456" ,
-    avatar: "https://images.tcdn.com.br/img/img_prod/740836/cadeira_gamer_concordia_gm3_rgb_com_controle_e_powerbank_10803_1_20a776245ed6e9b1bd655072771901e6.png"
+    avatar: "https://imgs.search.brave.com/dmNsyBvLmS4jetkOvFsxVmpaniEqqxT8BaNBAI-_7jM/rs:fit:416:416:1/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L2ljb24tb2YtYS1i/dXNpbmVzc21hbi1h/dmF0YXItb3ItcHJv/ZmlsZS1waWMtcGlj/dHVyZS1pZDQ3NDAw/MTg5Mj9rPTYmbT00/NzQwMDE4OTImcz0x/NzA2NjdhJnc9MCZo/PWF0cVpzV0YtVWNM/QkQ1dTJCTVpqcE11/cjZKOW56aVFyclBh/aXFaaDU3S1k9"
 }
 )
 
